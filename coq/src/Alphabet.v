b9066a4fcf8bb1f35609f4b5a8acc9854aaa904e
Mariam Katamashvili <katamashvili.mariami@gmail.com>
2026-05-30 11:48:04 +0400

    finilize alphabet definition

--- coq/src/Alphabet.v
+++ coq/src/Alphabet.v
@@ -1,29 +1,46 @@
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+From mathcomp Require Import ssreflect ssrbool eqtype seq.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Module Type OSYM.
+(* A plain alphabet of symbols. This is the minimal interface needed for
+   words, languages, regular-expression syntax, and semantics. *)
+Module Type SYM.
   Parameter A : eqType.
+End SYM.
+
+(* A plain alphabet plus an ordering on symbols. This is not part of the paper's
+   basic alphabet definition; it is only needed to sort symbols while building
+   canonical regular expressions. *)
+Module Type OSYM.
+  Include SYM.
 
   Parameter cmpA : A -> A -> comparison.
 
-  Axiom cmpA_refl : forall a, cmpA a a = Eq.
   Axiom cmpA_eq_axiom : forall a b, reflect (a = b)
     (if cmpA a b is Eq then true else false).
-  Axiom cmpA_trans : forall a b c (x : comparison),
-      cmpA a b = x -> cmpA b c = x -> cmpA a c = x.
-  Axiom cmpA_neg : forall a b, cmpA b a = CompOpp (cmpA a b).
-
-  Definition leA : rel A :=
-    fun a b => if cmpA a b is Gt then false else true.
-
-  Lemma leA_refl : reflexive leA.
-  Proof. by move=> a; rewrite /leA cmpA_refl. Qed.
-
-  Lemma leA_total : total leA.
-  Proof.
-    move=> a b; rewrite /leA cmpA_neg; by case: (cmpA b a).
-  Qed.
 End OSYM.
+
+(* A finite nonempty alphabet. Enumerates all symbols exactly once. *)
+Module Type FINSYM.
+  Include SYM.
+
+  Parameter alphabet : seq A.
+
+  Axiom alphabet_uniq : uniq alphabet.
+  Axiom alphabet_nonempty : alphabet != [::].
+  Axiom alphabet_complete : forall a : A, a \in alphabet.
+End FINSYM.
+
+(* A finite alphabet that also has an ordering on symbols. Automata construction
+   uses the finite list to enumerate transitions and canonicalization uses the
+   comparison to sort regular expressions. *)
+Module Type OFINSYM.
+  Include FINSYM.
+
+  Parameter cmpA : A -> A -> comparison.
+
+  Axiom cmpA_eq_axiom : forall a b, reflect (a = b)
+    (if cmpA a b is Eq then true else false).
+End OFINSYM.
